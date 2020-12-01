e732bfc8777123bcaf263d211a583d4af92068d0
Ankit Gupta <71442866+Ankit3012@users.noreply.github.com>
2020-12-01 15:28:16 +0530

    Update face-eye-detector.py

--- face-eye-detector.py
+++ face-eye-detector.py
@@ -1,11 +1,7 @@
 import numpy as np
 import cv2
 import dlib
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 hand_cascade= cv2.CascadeClassifier('hands.xml')
 cap = cv2.VideoCapture(0)
